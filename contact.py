cb9f4e3e90f4793457da330d2cb8c7823a780369
Наталья Бальчугова <balchugova98@bk.ru>
2024-02-04 23:12:19 +0700

    Create test add contact(homework)

---
+++ contact.py
@@ -0,0 +1,7 @@
+class Contact:
+
+    def __init__(self, firstname, lastname, address, work):
+        self.firstname = firstname
+        self.lastname = lastname
+        self.address = address
+        self.work = work
