0dab473b0077f7405cdb96da013a28e5fdcce289
Наталья Бальчугова <balchugova98@bk.ru>
2024-02-04 16:13:59 +0700

    Create  methods

--- test_add_group.py
+++ test_add_group.py
@@ -12,19 +12,35 @@ class TestAddGroup(unittest.TestCase):
         self.wd.implicitly_wait(30)
 
     def test_add_group(self):
-        wd = self.wd
-        wd.get("http://localhost/addressbook/")
+        wd = self.open_home_page()
+        self.login(wd)
+        self.open_groups_page(wd)
+        self.create_group(wd)
+        self.logout(wd)
+        # time.sleep(10)
+
+    def open_home_page(self):
+        wd = self.open_home_page()
+        self.open_home_page(wd)
+        return wd
+    def login(self, wd):
         wd.find_element(By.NAME, "user").send_keys("admin")
         wd.find_element(By.NAME, "pass").send_keys("secret")
         wd.find_element(By.XPATH, "//input[@value='Login']").click()
-        wd.find_element(By.LINK_TEXT, "groups").click()
+    def create_group(self, wd):
+        # init group creation
         wd.find_element(By.XPATH, "//input[@value='New group']").click()
+        # fill group form
         wd.find_element(By.NAME, "group_name").send_keys("test")
         wd.find_element(By.NAME, "group_header").send_keys("1234")
         wd.find_element(By.NAME, "group_footer").send_keys("@#$%")
         wd.find_element(By.XPATH, "//input[@value='Enter information']").click()
+    def open_groups_page(self, wd):
+        wd.find_element(By.LINK_TEXT, "groups").click()
+
+    def logout(self, wd):
+        # Logout
         wd.find_element(By.LINK_TEXT, "Logout").click()
-        # time.sleep(10)
 
     def is_element_present(self, how, what):
         try:
