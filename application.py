6db033f92961b1f173507bedaae527afa58d6286
Наталья Бальчугова <balchugova98@bk.ru>
2024-02-11 02:47:55 +0700

    Create fixture

---
+++ application.py
@@ -0,0 +1,42 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoAlertPresentException
+import unittest
+from group import Group
+
+class Application:
+    def __init__(self):
+        self.wd = webdriver.Chrome()
+        self.wd.implicitly_wait(30)
+
+    def open_home_page(self):
+        wd = self.wd
+        wd.get("http://localhost/addressbook/")
+
+    def login(self, user_name, password):
+        wd = self.wd
+        self.open_home_page()
+        wd.find_element(By.NAME, "user").send_keys(user_name)
+        wd.find_element(By.NAME, "pass").send_keys(password)
+        wd.find_element(By.XPATH, "//input[@value='Login']").click()
+
+    def create_group(self, group):
+        wd = self.wd
+        wd.find_element(By.XPATH, "//input[@value='New group']").click()
+        wd.find_element(By.NAME, "group_name").send_keys(group.name)
+        wd.find_element(By.NAME, "group_header").send_keys(group.header)
+        wd.find_element(By.NAME, "group_footer").send_keys(group.footer)
+        wd.find_element(By.XPATH, "//input[@value='Enter information']").click()
+
+    def open_groups_page(self):
+        wd = self.wd
+        wd.find_element(By.LINK_TEXT, "groups").click()
+
+    def logout(self):
+        wd = self.wd
+        wd.find_element(By.LINK_TEXT, "Logout").click()
+
+    def destroy(self):
+        self.wd.quit()
+
--- test_add_group.py
+++ test_add_group.py
@@ -1,79 +1,25 @@
-# -*- coding: utf-8 -*-
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import NoAlertPresentException
-import unittest
+# -*- coding: utf-8 -*
+import pytest
 from group import Group
+from application import Application
 
 
-class TestAddGroup(unittest.TestCase):
-    def setUp(self):
-        self.wd = webdriver.Chrome()
-        self.wd.implicitly_wait(30)
-
-    def test_add_group(self):
-        self.login(user_name="admin", password="secret")
-        self.open_groups_page()
-        self.create_group(Group(name="test", header="1234", footer="@#$%"))
-        self.logout()
-        # time.sleep(10)
-
-    def test_add_empty_group(self):
-        self.login(user_name="admin", password="secret")
-        self.open_groups_page()
-        self.create_group(Group(name="", header="", footer=""))
-        self.logout()
-        # time.sleep(10)
-
-    def open_home_page(self):
-        wd = self.wd
-        wd.get("http://localhost/addressbook/")
-
-    def login(self, user_name, password):
-        wd = self.wd
-        self.open_home_page()
-        wd.find_element(By.NAME, "user").send_keys(user_name)
-        wd.find_element(By.NAME, "pass").send_keys(password)
-        wd.find_element(By.XPATH, "//input[@value='Login']").click()
-
-    def create_group(self, group):
-        wd = self.wd
-        wd.find_element(By.XPATH, "//input[@value='New group']").click()
-        wd.find_element(By.NAME, "group_name").send_keys(group.name)
-        wd.find_element(By.NAME, "group_header").send_keys(group.header)
-        wd.find_element(By.NAME, "group_footer").send_keys(group.footer)
-        wd.find_element(By.XPATH, "//input[@value='Enter information']").click()
-    def open_groups_page(self):
-        wd = self.wd
-        wd.find_element(By.LINK_TEXT, "groups").click()
-
-    def logout(self):
-        wd = self.wd
-        wd.find_element(By.LINK_TEXT, "Logout").click()
-
-    def is_element_present(self, how, what):
-        try:
-            self.wd.find_element(by=how, value=what)
-        except NoSuchElementException:
-            return False
-        return True
-    
-    def is_alert_present(self):
-        try:
-            self.is_alert_present()
-            # self.wd.switch_to.alert()
-        except NoAlertPresentException:
-            return False
-        return True
-
-    def tearDown(self):
-        self.wd.quit()
+@pytest.fixture
+def app(request):
+    fixture = Application()
+    return fixture
 
 
-if __name__ == "__main__":
-    unittest.main()
+def test_add_group(app):
+    app.login(user_name="admin", password="secret")
+    app.open_groups_page()
+    app.create_group(Group(name="test", header="1234", footer="@#$%"))
+    app.logout()
+    # time.sleep(10)
 
 
-
-
+def test_add_empty_group(app):
+    app.login(user_name="admin", password="secret")
+    app.open_groups_page()
+    app.create_group(Group(name="", header="", footer=""))
+    app.logout()
