e35f0911a356ba996849a862d7fb127189c4f096
Наталья Бальчугова <balchugova98@bk.ru>
2024-02-25 02:04:07 +0700

    Receiving lists of groups, comparing lists of groups

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -5,10 +5,17 @@ from fixture.application import Application
 
 def test_add_group(app):
     # app.group.open_groups_page()
+    old_groups = app.group.get_groups_list()
     app.group.create(Group(name="test", header="1234", footer="@#$%"))
+    new_groups = app.group.get_groups_list()
+    assert len(old_groups) + 1 == len(new_groups)
     # time.sleep(10)
 
 
 def test_add_empty_group(app):
     # app.group.open_groups_page()
+    old_groups = app.group.get_groups_list()
     app.group.create(Group(name="", header="", footer=""))
+    new_groups = app.group.get_groups_list()
+    assert len(old_groups) + 1 == len(new_groups)
+
--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -2,9 +2,12 @@ from model.group import Group
 
 
 def test_modify_group_name(app):
+    old_groups = app.group.get_groups_list()
     if app.group.count() == 0:
         app.group.create(Group(name="test", header="1", footer="2"))
     app.group.modify_first_group(Group(name="New group", header="1234", footer="@#$%"))
+    new_groups = app.group.get_groups_list()
+    assert len(old_groups) == len(new_groups)
 
 # def test_modify_group_header(app):
 #     app.session.login(user_name="admin", password="secret")
