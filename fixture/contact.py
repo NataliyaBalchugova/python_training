469268c1281adc855b68c9037d78844a720599f9
Наталья Бальчугова <balchugova98@bk.ru>
2024-03-04 23:11:01 +0700

    Precondition if no contact exists (test_del_contact)

--- fixture/contact.py
+++ fixture/contact.py
@@ -1,7 +1,4 @@
 from selenium.webdriver.common.by import By
-#from model.contact import Contact
-#from fixture.session import SessionHelper
-# from fixture.application import Application
 
 
 class ContactHelper:
@@ -22,9 +19,17 @@ class ContactHelper:
         wd.find_element(By.XPATH, "//div[@id='content']/form/input[21]").click()
         wd.find_element(By.LINK_TEXT, "home").click()
 
+    def create_contact(self, contact):
+        wd = self.app.wd
+        wd.find_element(By.LINK_TEXT, "add new").click()
+        wd.find_element(By.NAME, "firstname").send_keys(contact.firstname)
+        wd.find_element(By.NAME, "lastname").send_keys(contact.lastname)
+        wd.find_element(By.NAME, "address").send_keys(contact.address)
+        wd.find_element(By.NAME, "work").send_keys(contact.work)
+        wd.find_element(By.XPATH, "//div[@id='content']/form/input[21]").click()
+        #wd.find_element(By.LINK_TEXT, "home").click()
 
     def delete_first_contact(self):
-        #wd = self.app.wd
         self.open_contact_page()
         self.select_contact()
         self.delete_contact()
@@ -35,6 +40,7 @@ class ContactHelper:
         wd.find_element(By.XPATH, "//input[@value='Delete']").click()
         alert = wd.switch_to.alert
         alert.accept()
+        self.open_contact_page()
 
     def select_contact(self):
         wd = self.app.wd
@@ -43,13 +49,15 @@ class ContactHelper:
 
     def modify_firstname_contact(self, new_contact_data):
         wd = self.app.wd
+        self.open_contact_page()
         # edit contact
-        wd.find_element(By.XPATH, "/html/body/div/div[4]/form[2]/table/tbody/tr[2]/td[8]/a/img").click()
-        #wd.find_element(By.CSS_SELECTOR, "a[href='edit.php?id=66']").click()
+        # edit_link = wd.find_element(By.NAME, 'selected[]').get_attribute('id')
+        # wd.get(f"http://localhost/addressbook/edit.php?id={edit_link}")
+        wd.find_element(By.XPATH, '//img[@title="Edit"]').click()
         # fill new data for firstname
         self.fill_contact_form(new_contact_data)
         # update contact
-
+        # /html/body/div/div[4]/form[1]/input[1]
         wd.find_element(By.XPATH, "//input[@value='Update']").click()
         # return to contacts page
         wd.find_element(By.LINK_TEXT, "home").click()
@@ -65,8 +73,18 @@ class ContactHelper:
             wd.find_element(By.NAME, field_name).clear()
             wd.find_element(By.NAME, field_name).send_keys(text)
 
+    def count_contacts(self):
+        wd = self.app.wd
+        self.open_contact_page()
+        # if not (wd.current_url.endswith("/group.php") and len(wd.find_elements(By.NAME, "new")) > 0):
+        # if not (wd.current_url.endswith("/group.php")):
+        # self.open_contact_page()
+        return len(wd.find_elements(By.NAME, "selected[]"))
 
+    def el_exist(self):
+        wd = self.app.wd
+        return (wd.find_elements(By.NAME, "selected[]"))
 
-
-
-
+    # def numb_res(self):
+    #     wd = self.app.wd
+    #     return (wd.find_element(By.ID, 'search_count'))
--- test/test_del_contact.py
+++ test/test_del_contact.py
@@ -1,4 +1,12 @@
 from model.contact import Contact
-def test_del_contact(app):
+
+
+def test_del_first_contact(app):
+    # if app.contact.count_contacts() == '0':
+    if app.contact.el_exist():
+        pass
+    else:
+        app.contact.create_contact(Contact(firstname="Alexandr", lastname="Ponomarev", work="System Administrator",
+                                           address="Demakova, 5"))
     app.contact.delete_first_contact()
 
