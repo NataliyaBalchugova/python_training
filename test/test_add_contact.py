e8dc8d591824d4174a451b173a738cdd4525c492
Наталья Бальчугова <balchugova98@bk.ru>
2024-03-09 21:50:12 +0700

    Comparison of contact lists before and after adding

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -2,5 +2,8 @@ from model.contact import Contact
 
 
 def test_add_contact(app):
-    app.contact.fill_contact(Contact(firstname="Alexandr", lastname="Ponomarev", work="System Administrator",
-                                      address="Demakova, 5"))
+    old_contacts = app.contact.get_contact_list()
+    app.contact.fill_contact(Contact(firstname="Alexandr", lastname="Ponomarev", work="System Administrator", address="Demakova, 5"))
+    new_contacts = app.contact.get_contact_list()
+    assert len(old_contacts) + 1 == len(new_contacts)
+
