56d55f3c109415c3c1b591c43ec6a99aa05b9763
Наталья Бальчугова <balchugova98@bk.ru>
2024-03-19 23:44:31 +0700

    Test parameterization

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,17 +1,27 @@
 # -*- coding: utf-8 -*
 from model.group import Group
+import pytest
+import random
+import string
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + string.punctuation + " "*10
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
 
+testdata = [Group(name="", header="", footer="")] + [
+    Group(name=random_string("name", 10), header=random_string("header", 20), footer=random_string("footer", 20))
+    for i in range(5)
+]
 
-def test_add_group(app):
+@pytest.mark.parametrize("group", testdata, ids=[repr(x) for x in testdata])
+def test_add_group(app, group):
     app.group.open_groups_page()
     old_groups = app.group.get_group_list()
-    group = Group(name="test", header="1234", footer="@#$%")
     app.group.create(group)
     assert len(old_groups) + 1 == app.group.count()
     new_groups = app.group.get_group_list()
     old_groups.append(group)
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
-    # time.sleep(10)
+
 
 # def test_add_empty_group(app):
     # app.group.open_groups_page()
