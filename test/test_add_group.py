31ca2634c88f2f4666cbe3f05c880dbc026b567b
n.balchugova <n.balchugova@staffcop.ru>
2024-04-11 14:40:59 +0700

    Fix generate test_add_group

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*
+from generator.group import generate
 from model.group import Group
+json_groups = generate()
 
 def test_add_group(app, json_groups):
     group = json_groups
@@ -7,7 +9,7 @@ def test_add_group(app, json_groups):
     app.group.open_groups_page()
     old_groups = app.group.get_group_list()
     app.group.create(group)
-    # assert len(old_groups) + 1 == app.group.count()
+    assert len(old_groups) + 1 == app.group.count()
     new_groups = app.group.get_group_list()
     old_groups.append(group)
     # assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
