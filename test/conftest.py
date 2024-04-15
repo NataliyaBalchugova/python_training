f1049f0862056c9a5050e798d181e520f7d13656
Наталья Бальчугова <balchugova98@bk.ru>
2024-04-15 08:55:20 +0700

    fix test db matches

--- test/conftest.py
+++ test/conftest.py
@@ -47,7 +47,7 @@ def stop(request):
 def pytest_addoption(parser):
     parser.addoption("--browser", action="store", default="chrome")
     parser.addoption("--target", action="store", default="target.json")
-
+    parser.addoption("--check_ui", action="store_true")
 
 def pytest_generate_tests(metafunc):
     for fixture in metafunc.fixturenames:
@@ -75,6 +75,11 @@ def db(request):
         dbfixture.destroy()
     request.addfinalizer(fin)
     return dbfixture
+@pytest.fixture
+def check_ui(request):
+    return request.config.getoption("--check_ui")
+
+
 
 
 
