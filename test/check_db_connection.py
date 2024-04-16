4340f435a055b7fccccc6da558f031447eed6ac1
Наталья Бальчугова <balchugova98@bk.ru>
2024-04-16 22:36:21 +0700

    Create def get_contacts_in_group, get_contacts_not_in_group

--- test/check_db_connection.py
+++ test/check_db_connection.py
@@ -1,5 +1,6 @@
 import pymysql.cursors
 from fixture.orm import ORMFixture
+from model.group import Group
 
 
 db = ORMFixture(host="127.0.0.1", name="addressbook", user="root", password="")
@@ -14,8 +15,25 @@ db = ORMFixture(host="127.0.0.1", name="addressbook", user="root", password="")
 #    pass #db.destroy()
 
 
+#try:
+#    l = db.get_contact_list()
+#    for item in l:
+#        print(item)
+#    print(len(l))
+#finally:
+#    pass #db.destroy()
+
+
+#try:
+#    l = db.get_contacts_in_group(Group(id="412"))
+#    for item in l:
+#        print(item)
+#    print(len(l))
+#finally:
+#    pass #db.destroy()
+
 try:
-    l = db.get_contact_list()
+    l = db.get_contacts_not_in_group(Group(id="412"))
     for item in l:
         print(item)
     print(len(l))
